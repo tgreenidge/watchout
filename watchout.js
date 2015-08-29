a9b008316e4ac3ee8284666da3b865b77f065a7b
Tisha Greenidge <tisha.m.greenidge@gmail.com>
2015-08-28 20:27:36 -0400

    moved androids to random locations

--- watchout.js
+++ watchout.js
@@ -1,2 +1,23 @@
 // start slingin' some d3 here.
 
+var moveEnemies = function(){
+  var newCoordinates = [];
+  for(var i = 0; i < 20; i++){
+    var temp = [];
+    temp.push(Math.random() * 840 + 30);
+    temp.push(Math.random() * 520 + 30);
+    newCoordinates.push(temp);
+  }
+
+  //select all enemies
+  d3.selectAll(".enemy")
+  .data(newCoordinates)
+  .transition()
+  .attr("x", function(d) { return d[0]; })
+  .attr("y", function(d) { return d[1]; })
+  .duration(500)
+  .ease("cubic-in-out");
+};
+
+//call moveEnemies (setInterval)
+setInterval( moveEnemies , 1000);
