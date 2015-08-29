e1ae0b316b8142cab8d3d0a13f9d9376aafe2803
Tisha Greenidge <tisha.m.greenidge@gmail.com>
2015-08-29 17:49:40 -0400

    detected collision for asteroid

--- watchout.js
+++ watchout.js
@@ -1,23 +1,8 @@
 // start slingin' some d3 here.
-var enemies = d3.selectAll(".enemy");
-
-var getCoordinates = function(){
-var newCoordinates = [];
-  for(var i = 0; i < 20; i++){
-    var temp = [];
-    temp.push(Math.random() * 840 + 30);
-    temp.push(Math.random() * 520 + 30);
-    newCoordinates.push(temp);
-  }
-  return newCoordinates;
-};
-
 var moveEnemies = function(node){
-  var newCoordinates = getCoordinates();
-  node.data(newCoordinates)
-  .transition()
-  .attr("x", function(d) { return d[0]; })
-  .attr("y", function(d) { return d[1]; })
+  node.transition()
+  .attr("x",  Math.random() * 840 + 30 )
+  .attr("y",  Math.random() * 520 + 30 )
   .duration(1000)
   .ease("cubic-in-out")
   .each("end", function(){
@@ -25,10 +10,46 @@ var moveEnemies = function(node){
   });
 };
 
-moveEnemies(enemies);
+var playerX = 800;
+var playerY = 450;
 
 var drag = d3.behavior.drag()
-  .on("drag", function() {circle.attr("cx", d3.event.x)
-                                .attr("cy", d3.event.y);});
+  .on("drag", function() {player.attr("cx", d3.event.x)
+                                .attr("cy", d3.event.y);
+                          playerX = d3.event.x;
+                          playerY = d3.event.y;});
+var player = d3.select(".player").call(drag);
+
+var collision = false;
+var checkCollision = function() {
+  var locations =[];
+  d3.selectAll(".enemy").each(function(d) {
+    var temp = [];
+    temp.push(Number(d3.select(this).attr("x")));
+    temp.push(Number(d3.select(this).attr("y")));
+    locations.push(temp);
+  });
+  for (var i = 0; i < locations.length; i++) {
+    var topBound = locations[i][1] - 20;
+    var bottomBound = locations[i][1] + 20;
+    var leftBound = locations[i][0] - 20;
+    var rightBound = locations[i][0] + 20;
+    if ((playerX > leftBound && playerX < rightBound) && 
+      (playerY > topBound && playerY < bottomBound)) {
+      collision = true;
+    } 
+  }
+};
+
+moveEnemies(d3.selectAll(".enemy"));
+d3.timer(checkCollision);
+setInterval(function(){
+  if(collision){
+    console.log("collision");
+    collision = false;
+  }
+}, 500);
+
   
-var circle = d3.select(".player").call(drag);
+
+
